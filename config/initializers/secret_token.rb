7ea58bfc7f8a7ffa4bf718e8f3045394070cdd04
Aaron Renner <aaron@animascodelabs.com>
2013-11-16 13:54:39 -0700

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ESensai::Application.config.secret_key_base = '12d06c9a9ff12758944e549bba755cafeff8c43645863303fb68011e13839c859d876394c85870da6c55817db43d8bfcbc664623097d114e0dfc4518cf23aed1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ESensai::Application.config.session_store :cookie_store, key: '_e-sensai_session'
